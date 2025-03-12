bf2981d497d8346d4a17a84d4b502e188f95a76c
casey paul <casey.paul@atlasschool.com>
2025-03-12 18:28:42 -0500

    Betty documentation

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -1,7 +1,9 @@
 #include "binary_trees.h"
 
 /**
- *
+ * binary_tree_size - Function that measures the size of a binary tree
+ * @tree: pointer to the root of node of the tree to travers
+ * Return: Size of the binary tree plus  1
  */
 size_t binary_tree_size(const binary_tree_t *tree)
 {
